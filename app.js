4dd516cf1661e3b31f7d6383a2d5bc0d7010d2a2
unknown <j0h@example.com>
2021-04-27 21:37:58 +0300

    Adding Middleware, with use() method and next() function

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express'); // import express js
 
 const app = express(); //create an express application and store in a constant app like function
 
+app.use((req, res, next) => { //use() method defined be expressjs, it's allows to add new middleware function
+    console.log('In the middleware');
+    next(); //Allows the request to continue to the next middleware in line
+}); 
+
+app.use((req, res, next) => { //use() method defined be expressjs, it's allows to add new middleware function
+    console.log('In another middleware');
+}); 
+
 const server = http.createServer(app); //app here actually happens to be a valid request hendler,so we pass app to create a server
 //const server = http.createServer(routes);
  
