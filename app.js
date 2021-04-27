01561ee5fa33f6192e6f509746f365c26a5635cd
unknown <j0h@example.com>
2021-04-27 14:43:08 +0300

    Using the Node Modules system

--- app.js
+++ app.js
@@ -1,42 +1,10 @@
 const http = require('http'); //looks for a global module //imported
-const fs = require('fs'); //alows work with file system
+const routes = require('./routes');
 
+//we need send incoming request to route.js file
 
-const server = http.createServer( (req, res) => { //http. this is haw we access functions or so-called methods and properties on oject in JS (http object is imported from http module)
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') { //if this url match slash /
-        res.write('<html>');
-        res.write('<head><title> Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Sent</button></form></body>');
-        res.write('</html>');
-        return res.end(); //when is return the code witch are down after this return won't be excuted //retunt quit function executing
-    }
-
-    if (url === '/message' && method === 'POST') { //we want to be sure, that we hangling a GET request // receiving posted message
-        const body = [];    //because we read the request body
-        req.on('data', (chunk) => { //event listener .on() //listen to data
-          //console.log(chunk);  
-          body.push(chunk);//take a body and push every new element to it
-        }); //allows to listen to certain events 
-        return req.on('end', () => {                 
-          const parsedBody = Buffer.concat(body).toString(); //concat body chunks and convert to string     
-          console.log(parsedBody);
-          const message = parsedBody.split('=')[1];
-          fs.writeFile('message.txt', message, err => {
-            res.statusCode = 302; //302 stand for redirection
-            res.setHeader('Location', '/');
-            return res.end();
-          });
-        });        
-        
-    }
-    res.setHeader('Content-Type', 'text/html');  //this method allows to set a header for response
-    res.write('<html>');
-    res.write('<head><title> My First Page</title></head>');
-    res.write('<body><h1>Hello from Node.js Server!</h1></body>')
-    res.write('</html>');
-    res.end(); //done with creating that response
-}); 
-
+console.log(routes.someText); //example of multiple export
+const server = http.createServer(routes.handler); //routes handle  //execution function what's stored in routes
+//const server = http.createServer(routes);
+ 
 server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+
+//connection function between files
+ const requestHandler = (req, res) => { //req, res response and request object
+    //add because we use these methods below in if statement  
+    const url = req.url;           
+    const method = req.method;
+    if (url === '/') { //if this url match slash / //routing logic
+        res.write('<html>');
+        res.write('<head><title> Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Sent</button></form></body>');
+        res.write('</html>');
+        return res.end(); //when is return the code witch are down after this return won't be excuted //retunt quit function executing
+    }
+    
+    if (url === '/message' && method === 'POST') { //we want to be sure, that we hangling a GET request // receiving posted message
+        const body = [];    //because we read the request body
+        req.on('data', (chunk) => { //event listener .on() //listen to data
+          //console.log(chunk);  
+          body.push(chunk);//take a body and push every new element to it
+        }); //allows to listen to certain events 
+        return req.on('end', () => {                 
+          const parsedBody = Buffer.concat(body).toString(); //concat body chunks and convert to string     
+          console.log(parsedBody);
+          const message = parsedBody.split('=')[1];
+          fs.writeFile('message.txt', message, err => {
+            res.statusCode = 302; //302 stand for redirection
+            res.setHeader('Location', '/');
+            return res.end();
+          });
+        });      
+    }
+    res.setHeader('Content-Type', 'text/html');  //this method allows to set a header for response
+    res.write('<html>');
+    res.write('<head><title> My First Page</title></head>');
+    res.write('<body><h1>Hello from Node.js Server!</h1></body>')
+    res.write('</html>');
+    res.end(); //done with creating that response
+};
+
+//Several export methods
+
+//module.exports = requestHandler; //module is global object exposed by node
+
+//can be used another export syntax as object
+  // module.exports = {
+  //     handler:requestHandler,
+  //     someText: 'Some hard coded text'
+  // };
+
+  //explicitly define export
+  // module.exports.handler = requestHandler;
+  // module.exports.someText = 'Some hard coded text';
+
+//shortcut which suport by nodejs also
+handler = requestHandler;
+someText = 'Some hard coded text';
+
+
+
