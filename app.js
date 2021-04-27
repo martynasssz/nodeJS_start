57ebe0f35de44e9a370cfb89981e667eacb625ea
unknown <j0h@example.com>
2021-04-27 22:51:01 +0300

    How middleware works, express.js - looking behind the scenes. Add res.sen() method, set up server using express js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const http = require('http'); //looks for a global module //imported
+
+//const http = require('http'); //looks for a global module //imported //it doesn't need because use app.listen
 
 const express = require('express'); // import express js
 
@@ -11,9 +12,15 @@ app.use((req, res, next) => { //use() method defined be expressjs, it's allows t
 
 app.use((req, res, next) => { //use() method defined be expressjs, it's allows to add new middleware function
     console.log('In another middleware');
+    res.send('<h1>Hello from Express</h1>'); // send allow to send a response
 }); 
 
+/*
 const server = http.createServer(app); //app here actually happens to be a valid request hendler,so we pass app to create a server
 //const server = http.createServer(routes);
  
 server.listen(3000);
+*/
+
+//use only this offered by expressjs framework insteand const server and sever.listen(3000)
+app.listen(3000); //set up server
