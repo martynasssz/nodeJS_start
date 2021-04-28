befd162445a2701189cf8df1061b749ca0ec50ce
unknown <j0h@example.com>
2021-04-28 12:39:13 +0300

    Addig a 404 Error Page (with massage 'page no found')

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-
 const express = require('express'); // import express js
-
-const adminRoutes = require('./routes/admin'); //import admin routes
-const shopRoutes = require('./routes/shop'); //import admin routes
-
 const bodyParser = require('body-parser');
 
 const app = express(); //create an express application and store in a constant app like function
+
+const adminRoutes = require('./routes/admin'); //import admin routes
+const shopRoutes = require('./routes/shop'); //import admin routes
 
 app.use(bodyParser.urlencoded({extended:false})); //extended:false, because it shoud be parse non-default features
 
 // ---!!! Order ir matter! ---
 app.use(adminRoutes); //not calling it like  function just the object itself, the router object we're exporting this file
 app.use(shopRoutes);
+
+app.use((req, res, next) => { // catch all middleware //without path filter
+    res.status(404).send('<h1>Page not found</h1>'); //status method
+}); 
 
 //use only this offered by expressjs framework insteand const server and sever.listen(3000)
 app.listen(3000); //set up server
