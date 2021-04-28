ab33f928896db162f064ed95e58cff6aba127456
unknown <j0h@example.com>
2021-04-28 11:05:08 +0300

    Parsing incomng Resquest, installed body-parser package for body parsing (sent through a from)

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 
 const express = require('express'); // import express js
+const bodyParser = require('body-parser');
 
 const app = express(); //create an express application and store in a constant app like function
 
-app.use('/', (req, res, next) => { //use() method defined be expressjs, it's allows to add new middleware function
-    console.log('This always runs!');
-    next(); 
-});
+app.use(bodyParser.urlencoded({extended:false})); //extended:false, because it shoud be parse non-default features
 
-app.use('/add-product', (req, res, next) => { 
-    console.log('In another middleware1');
-    res.send('<h1>The "add product" Page</h1>'); 
+app.use('/add-product', (req, res, next) => {     
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');     
 }); 
 
-app.use('/', (req, res, next) => { //use() method defined be expressjs, it's allows to add new middleware function
-    console.log('In another middleware');
+//middleware thats handles requests to product
+app.use('/product', (req, res, next) => { 
+    //redirect and log to console
+    console.log(req.body); //req.body a new fied added by express //extracting what user has send
+    res.redirect('/'); //redirect to slash route after submit pressing
+});
+
+app.use('/', (req, res, next) => { //use() method defined be expressjs, it's allows to add new middleware function    
     res.send('<h1>Hello from Express</h1>'); // send allow to send a response
 }); 
 
