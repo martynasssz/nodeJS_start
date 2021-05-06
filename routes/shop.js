ad1967825748b103e958c1380c55ca1f7df6b213
unknown <j0h@example.com>
2021-05-06 16:49:21 +0300

    Passing data with POST Request. IMPORTANT

--- routes/shop.js
+++ routes/shop.js
@@ -13,6 +13,9 @@ router.get('/products', shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct); //: // this signals to express that it should not look for a route like products product ID but instead that this part here can be anything
 
 router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
+
 router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckout);
 
