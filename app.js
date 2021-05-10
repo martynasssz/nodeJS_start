940c2a15eb155906e569d0f30118186a1540314d
unknown <j0h@example.com>
2021-05-10 19:08:06 +0300

    Retreiving Data

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin'); //import admin data
 const shopRoutes = require('./routes/shop'); //import admin routes
 
-db.execute('SELECT * FROM products').then().catch();
+db.execute('SELECT * FROM products')
+    .then(result => {
+       console.log(result[0], result[1]);   
+    })
+    .catch(err => {
+       console.log(err); 
+    });
 
 app.use(bodyParser.urlencoded({extended:false})); //extended:false, because it shoud be parse non-default features
 app.use(express.static(path.join(__dirname,'public'))); //user should be able access the public path
