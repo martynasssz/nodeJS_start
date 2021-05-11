6d7676ef62cb413d489199c538266d0ef816b991
unknown <j0h@example.com>
2021-05-11 10:41:04 +0300

    Syncing JS Definitions to the database

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express'); // import express js
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 //const expressHbs = require('express-handlebars'); //import express handlebars
 
@@ -16,14 +16,23 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin'); //import admin data
 const shopRoutes = require('./routes/shop'); //import admin routes
 
-app.use(bodyParser.urlencoded({extended:false})); //extended:false, because it shoud be parse non-default features
-app.use(express.static(path.join(__dirname,'public'))); //user should be able access the public path
+app.use(bodyParser.urlencoded({ extended: false })); //extended:false, because it shoud be parse non-default features
+app.use(express.static(path.join(__dirname, 'public'))); //user should be able access the public path
 
 //outsources routes
 app.use('/admin', adminRoutes); //adminRoutes because changed export
 app.use(shopRoutes);
 
 app.use(errorController.get404); //get from error controller
+
+sequelize
+    .sync().then(result => {
+        //console.log(result);
+    })y
+    
+    .catch(err => {
+        console.log(err);
+    });
 
 //use only this offered by expressjs framework insteand const server and sever.listen(3000)
 app.listen(3000); //set up server
