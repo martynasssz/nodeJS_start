537db244be9fa44f1b8e4f814b047ad04d9528bb
unknown <j0h@example.com>
2021-05-11 12:18:50 +0300

    Getting a Single Product with the where condition

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,47 +1,55 @@
 const Product = require('../models/product'); //importing Product class
 const Cart = require('../models/cart');
 
-exports.getProducts = (req, res, next) => {   
+exports.getProducts = (req, res, next) => {
   Product.findAll()
-  .then(products => {
-    res.render('shop/index', {  //pass, inject into template products // render shop index.ejs
-      prods: products, 
-      pageTitle:'All Products', 
-      path:'/products',  
+    .then(products => {
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .catch(err => {
-    console.log(err);
-  });      
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
-      console.log(product);
-      res.render('shop/product-detail', {//passing product property  
-        product: product[0],
-        pageTitle: product.title, //et dinamicallyp product title
-        path: '/products' //path which we want to mark in navigation
-    });
-  })  
-    .catch(err => console.log(err));  
+  // Product.findAll({ where: { id: prodId } })
+  //   .then(products => {
+  //     res.render('shop/product-detail', {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => console.log(err));
+  Product.findByPk(prodId)
+    .then(product => {
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 //render from sequelize
 exports.getIndex = (req, res, next) => {
   Product.findAll()
-  .then(products => {
-    res.render('shop/index', {  //pass, inject into template products // render shop index.ejs
-      prods: products, //rows should be our products
-      pageTitle:'Shop', 
-      path:'/'  
+    .then(products => {
+      res.render('shop/index', {  //pass, inject into template products // render shop index.ejs
+        prods: products, //rows should be our products
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .catch(err => {
-    console.log(err);
-  });
 };
 
 exports.getCart = (req, res, next) => {
@@ -51,25 +59,25 @@ exports.getCart = (req, res, next) => {
       for (product of products) {
         const cartProductData = cart.products.find(
           prod => prod.id === product.id
-          );
+        );
         if (cartProductData) { //check if this given product is also stored in a cart
-          cartProducts.push({productData: product, qty: cartProductData.qty}); //cart products push and I will add the product I'm currently looking at in this iteration of this loop.
-        }   
+          cartProducts.push({ productData: product, qty: cartProductData.qty }); //cart products push and I will add the product I'm currently looking at in this iteration of this loop.
+        }
       }
       res.render('shop/cart', { //render a view inside callback function
-        path:'/cart',
+        path: '/cart',
         pageTitle: 'Your Cart',
         products: cartProducts
       });
     });
-  });  
+  });
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId; //extract data
   Product.findById(prodId, product => {
     Cart.addProduct(prodId, product.price);
-  }); 
+  });
   res.redirect('/cart');
 };
 
@@ -78,13 +86,13 @@ exports.postCartDeleteProduct = (req, res, next) => {
   Product.findById(prodId, product => {
     Cart.deleteProduct(prodId, product.price);
     res.redirect('/cart');
-  });  
+  });
 }
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', { //render view
-    path:'/orders',
-    pageTitle: 'Your orders' 
+    path: '/orders',
+    pageTitle: 'Your orders'
   });
 };
 
