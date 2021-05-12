b1bc2053df262fdf67ba7baadff311bebb8382c8
unknown <j0h@example.com>
2021-05-12 16:35:36 +0300

    Using magic Association Methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,13 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    Product.create({
-        title: title,
-        price: price,
-        imageUrl: imageUrl,
-        description: description
-    })
+    req.user
+        .createProduct({
+            title: title,
+            price: price,
+            imageUrl: imageUrl,
+            description: description
+        })
         .then(result => {
             // console.log(result);
             console.log('Created Product');
@@ -33,23 +34,23 @@ exports.postAddProduct = (req, res, next) => {
 exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit;
     if (!editMode) {
-      return res.redirect('/');
+        return res.redirect('/');
     }
     const prodId = req.params.productId;
     Product.findByPk(prodId)
-      .then(product => {
-        if (!product) {
-          return res.redirect('/');
-        }
-        res.render('admin/edit-product', {
-          pageTitle: 'Edit Product',
-          path: '/admin/edit-product',
-          editing: editMode,
-          product: product
-        });
-      })
-      .catch(err => console.log(err));
-  };
+        .then(product => {
+            if (!product) {
+                return res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            });
+        })
+        .catch(err => console.log(err));
+};
 
 //sequelize syntax for saving updaed product 
 exports.postEditProduct = (req, res, next) => {
@@ -59,44 +60,44 @@ exports.postEditProduct = (req, res, next) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
     Product.findByPk(prodId)
-      .then(product => {
-        product.title = updatedTitle;
-        product.price = updatedPrice;
-        product.description = updatedDesc;
-        product.imageUrl = updatedImageUrl;
-        return product.save();
-      })
-      .then(result => {
-        console.log('UPDATED PRODUCT!');
-        res.redirect('/admin/products');
-      })
-      .catch(err => console.log(err));
-  };
+        .then(product => {
+            product.title = updatedTitle;
+            product.price = updatedPrice;
+            product.description = updatedDesc;
+            product.imageUrl = updatedImageUrl;
+            return product.save();
+        })
+        .then(result => {
+            console.log('UPDATED PRODUCT!');
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
+};
 
 //check products and render my view
 exports.getProducts = (req, res, next) => {
     Product.findAll()
-    .then(products => {
-      res.render('admin/products', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/admin/products'
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-}; 
+        .then(products => {
+            res.render('admin/products', {
+                prods: products,
+                pageTitle: 'All Products',
+                path: '/admin/products'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
     Product.findByPk(prodId)
-      .then(product => {
-        return product.destroy();
-      })
-      .then(result => {
-        console.log('DESTROYED PRODUCT');
-        res.redirect('/admin/products');
-      })
-      .catch(err => console.log(err));
-  };
+        .then(product => {
+            return product.destroy();
+        })
+        .then(result => {
+            console.log('DESTROYED PRODUCT');
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
+};
