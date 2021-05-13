d5a8c57cfd4f5d5cdc583cb93a3dd63bb874d3a5
unknown <j0h@example.com>
2021-05-13 21:07:04 +0300

    Setting a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,15 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req
+      .get('Cookie')
+      .split('=')[1];
     res.render('auth/login', {
-        path: '/login',
-        pageTitle: 'Login',
-        isAuthenticated: req.isLoggedIn
+      path: '/login',
+      pageTitle: 'Login',
+      isAuthenticated: isLoggedIn
     });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;  //store information in req object
+    res.setHeader('Set-Cookie', 'loggedIn=true'); // for saving loggedin
     res.redirect('/');
 };
-
